cd4e4af4cff62da7ff8423cf8b2856de5c5733e8
Maryna Parkhomenko <qwertf030915@gmail.com>
2025-04-01 18:33:55 +0300

    angular material has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [MatButton],
 })
 export class AppComponent {
   title = 'document-manager';
