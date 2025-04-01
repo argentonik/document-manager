fca8b36c5c0c08b460065e5d77695f3e3f075808
Maryna Parkhomenko <qwertf030915@gmail.com>
2025-04-01 23:28:35 +0300

    auth layout styles improvements

--- src/app/shared/layouts/auth-layout/auth-layout.component.ts
+++ src/app/shared/layouts/auth-layout/auth-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCard],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss',
 })
--- src/app/shared/layouts/main-layout/main-layout.component.ts
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCard],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
