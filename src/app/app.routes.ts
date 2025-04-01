11a865f4ae3111eb77f1b2c5e0e92f4648949654
Maryna Parkhomenko <qwertf030915@gmail.com>
2025-04-01 19:11:07 +0300

    structure improvement

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
-import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './features/auth/sign-up/sign-up.component';
+import { SignInComponent } from './features/auth/sign-in/sign-in.component';
 import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
