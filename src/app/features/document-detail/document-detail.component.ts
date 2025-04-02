9c9d0983e61f72a851ad1fc4cb3c5cd10b896656
Maryna Parkhomenko <qwertf030915@gmail.com>
2025-04-02 23:42:20 +0300

    basic pdf viewer has been added

--- src/app/features/document-detail/document-detail.component.ts
+++ src/app/features/document-detail/document-detail.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCard } from '@angular/material/card';
+import { PdfViewerComponent } from '../../pdf-viewer/pdf-viewer.component';
 
 @Component({
   selector: 'app-document-detail',
-  imports: [MatCard],
+  imports: [MatCard, PdfViewerComponent],
   templateUrl: './document-detail.component.html',
   styleUrl: './document-detail.component.scss',
 })
